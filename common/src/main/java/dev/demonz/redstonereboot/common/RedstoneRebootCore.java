2402f42606516a1996598d94cfde6d4835319cde
Cyrus <demonzdev@.com>
2026-04-10 18:56:29 +0000

    chore: bump version to 1.2.0 for config-version and modrinth features

--- common/src/main/java/dev/demonz/redstonereboot/common/RedstoneRebootCore.java
+++ common/src/main/java/dev/demonz/redstonereboot/common/RedstoneRebootCore.java
@@ -16,7 +16,7 @@ import org.slf4j.LoggerFactory;
  */
 public class RedstoneRebootCore {
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.2.0";
     public static final String BRAND = "RedstoneReboot";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BRAND);
