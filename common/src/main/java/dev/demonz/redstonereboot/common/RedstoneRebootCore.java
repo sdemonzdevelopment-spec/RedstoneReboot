3e5e42721c12c619cab296f2ac32eb9d3bc7d067
Cyrus <demonzdev@.com>
2026-04-13 19:04:08 +0000

    workflows enhancments

--- common/src/main/java/dev/demonz/redstonereboot/common/RedstoneRebootCore.java
+++ common/src/main/java/dev/demonz/redstonereboot/common/RedstoneRebootCore.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
  */
 public class RedstoneRebootCore {
 
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.3.1";
     public static final String BRAND = "RedstoneReboot";
 
     private static final Logger LOGGER = Logger.getLogger(BRAND);
